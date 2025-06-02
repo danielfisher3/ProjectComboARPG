7f35f2cff97356cdbb61e7dcf0b8fec9272f90b2
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2025-06-02 18:12:43 -0500

    Fixing attributes file

--- ProjectComboARPG - Copy/Source/ProjectComboARPG/ProjectComboARPG.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjectComboARPG.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectComboARPG, "ProjectComboARPG" );
