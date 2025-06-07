316a166072f9faafdb7543a60669265b67578ac9
danielfisher3 <38573896+danielfisher3@users.noreply.github.com>
2025-06-07 09:55:30 -0500

    Weapon
    After playing around with the one DT weapon class, realized multiple would be more optimal/cleaner-

---
+++ ProjectComboARPG - Copy/Source/ProjectComboARPG/Public/DTObjectClasses/DTOneHandWeapon.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Engine/DataTable.h"
+#include "WeaponClassification.h"
+#include "DTOneHandWeapon.generated.h"
+
+USTRUCT(BlueprintType)
+struct FOneHandedWeap : public FTableRowBase 
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EOneHandedWeaponType OHType = EOneHandedWeaponType::EOHWT_MAX;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString WeaponName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Damage;
+};
+
+
+UCLASS()
+class PROJECTCOMBOARPG_API UDTOneHandWeapon : public UObject
+{
+	GENERATED_BODY()
+
+	
+	
+};
